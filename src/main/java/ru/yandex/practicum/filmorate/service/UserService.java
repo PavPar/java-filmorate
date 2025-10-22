b34c46e11a689babf139000089326f92b7ccb898
vorona_af <vorona_af@magnit.ru>
2025-10-22 22:59:00 +0300

    Функциональность Лента событий

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -10,7 +10,6 @@ import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class UserService {
